c010f49a1a1543be82bd3e673952071613d6046b
jpziegner <jziegner23@gmail.com>
2022-06-03 09:11:45 -0400

    tictactoe

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
+const winningCombos = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
+const board = ['','','','','','','','','']
+
 
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const squaredEls = document.getElementById('sq0','sq1','sq2','sq3','sq4','sq5','sq6','sq7','sq8')
+const messageEl = document.getElementById('message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -15,4 +26,7 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
-
+function init() {
+ 
+}
+console.log(init())
